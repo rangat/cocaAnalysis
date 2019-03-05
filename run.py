72969ce72c2a547963b86308d1a9adc2c9ecac87
Rangaraj Tirumala <rangaraj.t@gmail.com>
2019-03-05 01:00:07 -0500

    added timer to run script

--- run.py
+++ run.py
@@ -2,7 +2,13 @@ import loadJSON
 import json_csv
 import collect_dataset
 
+import time
+start = time.time()
+
 loadJSON.run()
 json_csv.run()
 collect_dataset.run()
 json_csv.json_to_csv("edited/coca_data.json")
+
+end = time.time()
+print(end - start)
