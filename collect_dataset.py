e0bb60fa5f59a63dde3615d3d5506c91fad8a6d9
Rangaraj Tirumala <rangaraj.t@gmail.com>
2019-04-04 09:37:44 -0400

    created extra wrapper function to cleanly run all files I need

--- collect_dataset.py
+++ collect_dataset.py
@@ -10,8 +10,14 @@ def run():
             continue
         
         with open('edited/{}'.format(filename)) as json_data:
-            collected.extend(json.load(json_data))
+            data = json.load(json_data)
+            collected.extend(data)
+        
+        print(len(collected))
+        print(type(collected[0]))
 
     with open("edited/coca_data.json", 'w') as outfile:
-        json.dump(collected, outfile)
+        json.dump(collected, outfile, indent=4)
         print("successfully made complete json")
+
+run()
